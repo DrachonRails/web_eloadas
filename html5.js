906ab15746911300553b31214675fdc986b77ee9
Balázs <ablonczi.92@gmail.com>
2025-04-23 23:49:55 +0200

    Canvas javítás

--- html5.js
+++ html5.js
@@ -50,7 +50,9 @@
       }
   
       // Canvas rajzolás
+      document.addEventListener("DOMContentLoaded", function () {
       const canvas = document.getElementById("myCanvas");
       const ctx = canvas.getContext("2d");
       ctx.fillStyle = "green";
       ctx.fillRect(10, 10, 150, 75);
+    });
